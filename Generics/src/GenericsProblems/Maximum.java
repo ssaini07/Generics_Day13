d1d94f80e166897bc70052f3bb8769e1c7fea4ee
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-11 13:41:36 +0530

    Refractor 2 doen

--- Generics/src/GenericsProblems/Maximum.java
+++ Generics/src/GenericsProblems/Maximum.java
@@ -2,65 +2,38 @@ package GenericsProblems;
 
 import java.util.Scanner;
 
-public class Maximum {
+public class Maximum<T> {
 
-	// Method for finding maximum number (int, float, string)
+	T meth1;
+	T meth2;
+	T meth3;
 
-	public static <T extends Comparable<T>> T findMax(T a, T b, T c) {
+	public Maximum(T meth1, T meth2, T meth3) {
+		// super();
+		this.meth1 = meth1;
+		this.meth2 = meth2;
+		this.meth3 = meth3;
+	}
 
-		T max = a;
+	public static <T extends Comparable<T>> T findMax(T meth1, T meth2, T meth3) {
 
-		if (b.compareTo(max) > 0) {
-			max = b;
+		T max = meth1;
+
+		if (meth2.compareTo(max) > 0) {
+			max = meth2;
 		}
-		if (c.compareTo(max) > 0) {
-			max = c;
+		if (meth3.compareTo(max) > 0) {
+			max = meth3;
 		}
 		return max;
 	}
 
-	// Taking input as an integer
-	public void findMaxInteger() {
-		Maximum mx = new Maximum();
-		System.out.println("Enter 3 integer numbers");
-		Scanner sc = new Scanner(System.in);
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int num3 = sc.nextInt();
-
-		System.out.println("Your maximum integer number is:" + mx.findMax(num1, num2, num3));
-	}
-
-	// Taking input as a float
-	public void findMaxFloat() {
-		Maximum mx = new Maximum();
-		System.out.println("Enter 3 float numbers");
-		Scanner sc = new Scanner(System.in);
-
-		float num1 = sc.nextFloat();
-		float num2 = sc.nextFloat();
-		float num3 = sc.nextFloat();
-
-		System.out.println("Your maximum float number is:" + mx.findMax(num1, num2, num3));
-	}
-
-	// Take Input as String number
-	public void findMaxString() {
-		Maximum max = new Maximum();
-		System.out.println("Enter 3 String numbers");
-		Scanner sc = new Scanner(System.in);
-		String num1 = sc.next();
-		String num2 = sc.next();
-		String num3 = sc.next();
-
-		System.out.println("Your maximum String number is:" + max.findMax(num1, num2, num3));
-	}
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to Generic Practice Set");
-		Maximum max = new Maximum();
-		max.findMaxString();
-		max.findMaxInteger();
-		max.findMaxString();
+		Maximum max = new Maximum(args, args, args);
+		System.out.println("Welcome to Generic Practice Problem");
+		System.out.println("Max Integer is:" + max.findMax(7, 8, 9));
+		System.out.println("Max Float is:" + max.findMax(4.6, 5.3, 6.7));
+		System.out.println("Max String is:" + max.findMax("apple", "banana", "peach"));
+
 	}
 }
