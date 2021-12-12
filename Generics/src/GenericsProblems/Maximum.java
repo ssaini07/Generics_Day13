3eea0581be59b47543a34bbf95583dadf6981a84
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-12 11:06:56 +0530

    Uc4 done

--- Generics/src/GenericsProblems/Maximum.java
+++ Generics/src/GenericsProblems/Maximum.java
@@ -1,39 +1,26 @@
 package GenericsProblems;
+import java.util.Arrays;
+import java.util.List;
+public class Maximum<T extends Comparable<T>> {
 
-import java.util.Scanner;
+	public static <T extends Comparable<T>> T findMax(List<T> array) {
 
-public class Maximum<T> {
-
-	T meth1;
-	T meth2;
-	T meth3;
-
-	public Maximum(T meth1, T meth2, T meth3) {
-		// super();
-		this.meth1 = meth1;
-		this.meth2 = meth2;
-		this.meth3 = meth3;
-	}
-
-	public static <T extends Comparable<T>> T findMax(T meth1, T meth2, T meth3) {
-
-		T max = meth1;
-
-		if (meth2.compareTo(max) > 0) {
-			max = meth2;
-		}
-		if (meth3.compareTo(max) > 0) {
-			max = meth3;
+		T max = array.get(0);
+		for (T key : array) {
+			if (key.compareTo(max) > 0)
+				max = key;
 		}
 		return max;
 	}
 
 	public static void main(String[] args) {
-		Maximum max = new Maximum(args, args, args);
-		System.out.println("Welcome to Generic Practice Problem");
-		System.out.println("Max Integer is:" + max.findMax(7, 8, 9));
-		System.out.println("Max Float is:" + max.findMax(4.6, 5.3, 6.7));
-		System.out.println("Max String is:" + max.findMax("apple", "banana", "peach"));
+		Maximum max = new Maximum();
+		System.out.println("The Maximum integer num is:" + 
+		max.findMax(Arrays.asList(9, 12, 22, 76, 439, 29)));
+		System.out.println("The Maximum float num is:" + 
+		max.findMax(Arrays.asList(1.9, 32.9, 63.7, 22.6, 12.4, 11.3)));
+		System.out.println("The Maximum String  is:" + 
+		max.findMax(Arrays.asList("Apple", "Blueberry", "Orange", "Cauliflower", "Onion")));
 
 	}
 }
