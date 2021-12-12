5c2589abb7fd25ae778ccafd1bba6ac50770e129
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-12 11:29:47 +0530

    Done uc5

--- Generics/src/GenericsProblems/Maximum.java
+++ Generics/src/GenericsProblems/Maximum.java
@@ -1,9 +1,11 @@
 package GenericsProblems;
+
 import java.util.Arrays;
 import java.util.List;
+
 public class Maximum<T extends Comparable<T>> {
 
-	public static <T extends Comparable<T>> T findMax(List<T> array) {
+	public static <T extends Comparable<T>> T findMaxMethod(List<T> array) {
 
 		T max = array.get(0);
 		for (T key : array) {
@@ -13,14 +15,20 @@ public class Maximum<T extends Comparable<T>> {
 		return max;
 	}
 
+	public static <T extends Comparable> T printMax() {
+		System.out.println("Here calling findMaxMethod through printMax");
+		System.out.println("The Maximum integer num is:" + findMaxMethod(Arrays.asList(9, 12, 22, 76, 439, 29)));
+		System.out.println("The Maximum float num is:" + findMaxMethod(Arrays.asList(1.9, 32.9, 63.7, 22.6, 12.4, 11.3)));
+		System.out.println("The Maximum String  is:"
+				+ findMaxMethod(Arrays.asList("Apple", "Blueberry", "Orange", "Cauliflower", "Onion")));
+		return null;
+
+	}
+
 	public static void main(String[] args) {
-		Maximum max = new Maximum();
-		System.out.println("The Maximum integer num is:" + 
-		max.findMax(Arrays.asList(9, 12, 22, 76, 439, 29)));
-		System.out.println("The Maximum float num is:" + 
-		max.findMax(Arrays.asList(1.9, 32.9, 63.7, 22.6, 12.4, 11.3)));
-		System.out.println("The Maximum String  is:" + 
-		max.findMax(Arrays.asList("Apple", "Blueberry", "Orange", "Cauliflower", "Onion")));
+		System.out.println("the Max Value is: ");
+
+		printMax();
 
 	}
 }
